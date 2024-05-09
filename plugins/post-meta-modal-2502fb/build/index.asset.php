3cc723cb65cb899a0b3be0dbb50e54b105293a88
Nick Diego <nickmdiego@gmail.com>
2024-05-09 14:45:05 -0500

    Add Post Meta Modal example.

---
+++ plugins/post-meta-modal-2502fb/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'a58dfc01975a4a044c6d');
