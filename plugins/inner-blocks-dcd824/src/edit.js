b7d2513107e119ce16ebd3c61e8c757c1285d420
JuanMa <juanma.garrido@gmail.com>
2024-06-24 12:44:27 +0100

    improvements on inner block example (#121)

--- plugins/inner-blocks-dcd824/src/edit.js
+++ plugins/inner-blocks-dcd824/src/edit.js
@@ -2,12 +2,19 @@
  * WordPress Dependencies
  */
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import './editor.scss';
+
+const MY_TEMPLATE = [
+	[ 'core/image', {} ],
+	[ 'core/heading', { placeholder: 'Book Title' } ],
+	[ 'core/paragraph', { placeholder: 'Summary' } ],
+];
 
 const Edit = () => {
 	const blockProps = useBlockProps();
 	return (
 		<div { ...blockProps }>
-			<InnerBlocks />
+			<InnerBlocks template={ MY_TEMPLATE } />
 		</div>
 	);
 };
