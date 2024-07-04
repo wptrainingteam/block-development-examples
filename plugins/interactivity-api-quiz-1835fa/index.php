9865bc37df7687b6845b77d8698a8b92c9b7866e
JuanMa Garrido <juanma.garrido@gmail.com>
2024-07-04 07:39:22 +0100

    Fixed issue with accesing data defined in another component in PHP

--- plugins/interactivity-api-quiz-1835fa/index.php
+++ plugins/interactivity-api-quiz-1835fa/index.php
@@ -17,21 +17,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+// Setup.
+define( 'UP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
 /**
- * Automatically registers block types by scanning the build/blocks folder.
+ * Automatically registers block types by ascanning the build/blocks folder.
  *
  * This function searches for JSON files within each subfolder and registers
  * them as block types. It is triggered on WordPress 'init' action.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function interactivity_api_quiz_1835fa__register_block() {
-	if ( file_exists( __DIR__ . '/build/blocks/' ) ) {
-			$block_json_files = glob( __DIR__ . '/build/blocks/*/block.json' );
-		foreach ( $block_json_files as $filename ) {
-				$block_folder = dirname( $filename );
-				register_block_type( $block_folder );
-		}
+function interactivity_api_quiz_1835fa__register_blocks() {
+
+	$blocks = array(
+		array( 'name' => 'quiz-1835fa' ),
+		array( 'name' => 'quiz-progress-1835fa' ),
+	);
+
+	foreach ( $blocks as $block ) {
+		register_block_type(
+			UP_PLUGIN_DIR . 'build/blocks/' . $block['name']
+		);
 	}
 }
-add_action( 'init', 'interactivity_api_quiz_1835fa__register_block' );
+
+add_action( 'init', 'interactivity_api_quiz_1835fa__register_blocks' );
