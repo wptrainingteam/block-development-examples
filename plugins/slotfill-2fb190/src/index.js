d750fbd0eb09c3e7dad11acbf0840802dc367483
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-02 16:27:44 +0100

    updated slot fill example

---
+++ plugins/slotfill-2fb190/src/index.js
@@ -0,0 +1,5 @@
+import { registerSlotFillWithPluginDocumentSettingPanel } from './slotfill-PluginDocumentSettingPanel-2fb190';
+import { registerSlotFillWithPluginSidebar } from './slotfill-PluginSidebar-2fb190';
+
+registerSlotFillWithPluginDocumentSettingPanel();
+registerSlotFillWithPluginSidebar();
