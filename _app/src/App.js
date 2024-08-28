734ba70dd8a88b9fc5330f58b1f3f30c48ed4111
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-28 13:17:44 +0200

    DataViews app updates

--- _app/src/App.js
+++ _app/src/App.js
@@ -1,115 +1,128 @@
 import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
+import { getItemsForFilter } from './utils';
+import './styles.scss';
 
-import '@wordpress/dataviews/build-style/style.css';
-import '@wordpress/components/build-style/style.css';
-
-import EXAMPLES from './_data/examples.json';
-
-function App() {
-	const [ data, setData ] = useState( EXAMPLES );
-
-	const fields = [
-		{
-			id: 'slug',
-			header: 'Slug',
-			enableGlobalSearch: true,
-			enableSorting: true,
+import data from './_data/examples.json';
+const tagsForFilter = getItemsForFilter( 'tags' )( data );
+console.log( tagsForFilter );
+const fields = [
+	{
+		id: 'slug',
+		label: 'Slug',
+		enableGlobalSearch: true,
+		enableHiding: false,
+	},
+	{
+		id: 'folder',
+		label: 'Folder',
+		enableHiding: false,
+		render: ( { item } ) => {
+			return (
+				<span>
+					<a
+						target="_blank"
+						href={ `https://github.com/WordPress/block-development-examples/tree/trunk/plugins/${ item.slug }` }
+						rel="noreferrer"
+					>
+						📂
+					</a>
+				</span>
+			);
 		},
-		{
-			id: 'description',
-			header: 'Description',
-			enableGlobalSearch: true,
+	},
+	{
+		id: 'demo',
+		label: 'Demo',
+		enableHiding: false,
+		render: ( { item } ) => {
+			return (
+				<span>
+					<a
+						target="_blank"
+						href={ `https://playground.wordpress.net/?blueprint-url=https://raw.githubusercontent.com/WordPress/block-development-examples/trunk/plugins/${ item.slug }/_playground/blueprint.json` }
+						rel="noreferrer"
+					>
+						📺
+					</a>
+				</span>
+			);
 		},
-		{
-			id: 'tags',
-			header: 'Tags',
-			render: ( { item } ) => {
-				return item.tags.join( ', ' );
-			},
-			enableSorting: false,
+	},
+	{
+		id: 'zip',
+		label: 'ZIP',
+		enableHiding: false,
+		render: ( { item } ) => {
+			return (
+				<span>
+					<a
+						target="_blank"
+						href={ `https://raw.githubusercontent.com/WordPress/block-development-examples/deploy/zips/${ item.slug }.zip` }
+						rel="noreferrer"
+					>
+						📦
+					</a>
+				</span>
+			);
 		},
-	];
+	},
+	{
+		id: 'description',
+		label: 'Description',
+		enableGlobalSearch: true,
+	},
+	{
+		id: 'tags',
+		label: 'Tags',
+		elements: getItemsForFilter( 'tags' )( data ),
+		enableGlobalSearch: true,
+		enableSorting: false,
+		render: ( { item } ) => {
+			return (
+				<div class="tags_example">
+					{ item.tags.map( ( tag ) => (
+						<span>{ tag.toUpperCase() }</span>
+					) ) }
+				</div>
+			);
+		},
+		filterBy: {
+			isPrimary: true,
+		},
+	},
+];
 
-	const [ paginationInfo, setPaginationInfo ] = useState( {
-		totalItems: 0,
-		totalPages: 0,
-	} );
-	const [ view, setView ] = useState( {
-		type: 'table',
-		hiddenFields: [],
+const DEFAULT_VIEW = {
+	type: 'table',
+	hiddenFields: [],
+	perPage: 10,
+	layout: {
+		page: 1,
 		perPage: 10,
-		layout: {
-			page: 1,
-			perPage: 10,
-			layout: {},
-			columnFields: [],
-		},
-		filters: [],
-	} );
+		layout: {},
+		columnFields: [],
+	},
+	filters: [],
+	fields: [ 'slug', 'folder', 'demo', 'zip', 'description', 'tags' ],
+};
 
-	// const fields = [
-	// 	{
-	// 		id: 'imaxe_src',
-	// 		header: 'Imaxe',
-	// 		render: ( { item } ) => (
-	// 			<a title={ item.imaxe_autor } href={ item.imaxe_url }>
-	// 				<img
-	// 					width="512"
-	// 					alt="Praia de Silgar, Sanxenxo, Galiza"
-	// 					src={ item.imaxe_src }
-	// 				/>
-	// 			</a>
-	// 		),
-	// 		enableSorting: false,
-	// 	},
-	// 	{
-	// 		id: 'nome',
-	// 		header: 'Nome',
-	// 		enableGlobalSearch: true,
-	// 	},
-	// 	{
-	// 		id: 'provincia',
-	// 		header: 'Provincia',
-	// 		enableGlobalSearch: true,
-	// 		elements: PROVINCIAS,
-	// 		filterBy: {
-	// 			isPrimary: true,
-	// 			operators: [ 'is' ],
-	// 		},
-	// 	},
-	// 	{
-	// 		id: 'habitantes',
-	// 		header: 'Nro hab.',
-	// 		render: ( { item } ) => formatNumber( item.habitantes ),
-	// 		enableSorting: false, // filterSortAndPaginate cannot sort by number types yet.
-	// 	},
-	// 	{
-	// 		id: 'km_cadrados',
-	// 		header: 'Extensión (km2)',
-	// 		render: ( { item } ) => formatNumber( item.km_cadrados ),
-	// 		enableSorting: false, // filterSortAndPaginate cannot sort by number types yet.
-	// 	},
-	// ];
-
-	const onChangeView = ( view ) => {
-		const { data: newData, paginationInfo: newPaginationInfo } =
-			filterSortAndPaginate( EXAMPLES, view, fields );
-		setView( view );
-		setData( newData );
-		setPaginationInfo( newPaginationInfo );
-	};
+const App = () => {
+	const [ view, setView ] = useState( DEFAULT_VIEW );
+	const { data: processedData, paginationInfo } = useMemo( () => {
+		return filterSortAndPaginate( data, view, fields );
+	}, [ view ] );
 
 	return (
 		<DataViews
-			data={ data }
+			data={ processedData }
 			fields={ fields }
 			view={ view }
 			getItemId={ ( item ) => item.slug }
-			onChangeView={ onChangeView }
+			onChangeView={ setView }
 			paginationInfo={ paginationInfo }
 		/>
 	);
-}
+};
 
 export default App;
