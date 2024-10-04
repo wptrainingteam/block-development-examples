fb125e8960cee2357c96858543b48231d1d407c2
JuanMa Garrido <juanma.garrido@gmail.com>
2024-10-04 13:32:38 +0100

    cleaner version interactivity router view

--- plugins/interactivity-router-2f43f8/src/view.js
+++ plugins/interactivity-router-2f43f8/src/view.js
@@ -3,13 +3,6 @@ import { store } from '@wordpress/interactivity';
 const { state } = store( 'router-2f43f8', {
 	state: {
 		urlRegionDisplay: window.location.href,
-		get urlRegionDisplaySlug() {
-			const { pathname } = new URL( state.urlRegionDisplay );
-			const isHome = pathname === '/';
-			return isHome
-				? '/'
-				: '/' + pathname.split( '/' ).filter( Boolean ).pop();
-		},
 	},
 	actions: {
 		*navigate( e ) {
@@ -18,8 +11,6 @@ const { state } = store( 'router-2f43f8', {
 				'@wordpress/interactivity-router'
 			);
 			state.urlRegionDisplay = e.target.href;
-
-			//yield actions.navigate( state.urlRegionDisplaySlug );
 			yield actions.navigate( e.target.href );
 		},
 	},
