1fcaa1dc36669dc47fb7f751e93e7233fe987224
JuanMa Garrido <juanma.garrido@gmail.com>
2024-12-17 10:53:10 +0100

    JSON prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,16 @@
 // Import the default config file and expose it in the project root.
+// Configure arrays to display each element on a new line
+const config = {
+	...require( '@wordpress/prettier-config' ),
+	overrides: [
+		{
+			files: [ '*.json' ],
+			options: {
+				parser: 'json',
+				printWidth: 1,
+			},
+		},
+	],
+};
 // Useful for editor integrations.
-module.exports = require('@wordpress/prettier-config');
+module.exports = config;
