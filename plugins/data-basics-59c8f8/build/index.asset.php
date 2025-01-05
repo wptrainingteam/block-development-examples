7afc955efda8eee7c3e35a4fb0feef1988e80c2e
JuanMa Garrido <juanma.garrido@gmail.com>
2025-01-05 19:00:53 +0100

    test writeToPath blueprint

---
+++ plugins/data-basics-59c8f8/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-notices'), 'version' => '99cc9307aeb738cd8321');
